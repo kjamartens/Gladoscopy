f2b608080eac955a4ae7f0948adef17376afe199
Koen J.A. Martens <32260034+kjamartens@users.noreply.github.com>
2023-10-13 15:53:39 +0200

    Update LiveModeControlScripts.py

--- GUI/LiveModeControlScripts.py
+++ GUI/LiveModeControlScripts.py
@@ -8,16 +8,19 @@ import threading
 import time
 import queue
 from PyQt5.QtWidgets import QMainWindow
+from pycromanager import *
 # Define a flag to control the continuous task
 stop_continuous_task = False
 total_th1 = 0;
 lock = threading.Lock()
 
 # Create a queue to pass image data between threads
-image_queue = queue.Queue()
-
+image_queue_analysis = queue.Queue()
 # Create a signal to communicate between threads
 analysis_done_signal = pyqtSignal(object)
+
+image_queue_liveObtain = queue.Queue()
+liveObtain_done_signal = pyqtSignal(object)
 
 def drawRandomOverlay(layer_name):
     # create a list of polygons
@@ -46,6 +49,37 @@ def drawRandomOverlay(layer_name):
         face_color='red',
     )
             
+def drawTextOverlay(layer_name,textv):
+    polygons = [np.array([[0, 0], [10, 10], [20, 20], [30, 30]])]
+    #See if layer exists
+    shapeLayer = getLayerIdFromName(layer_name)
+
+    textstring = {
+        'string': 'TEST',
+        'anchor': 'upper_left',
+        'translation': [0, 0],
+        'size': 100,
+        'color': 'red',
+    }
+    
+    #Add a layer if it doesn't exist yet
+    if not shapeLayer:
+        shapes_layer = napariViewer.add_shapes(
+            polygons,
+            shape_type='polygon',
+            edge_width=5,
+            edge_color='yellow',
+            face_color='red',
+            name = layer_name,
+            text = textstring
+        )
+    else:
+        #delete info in the layer and go from there
+        napariViewer.layers[shapeLayer[0]].data = []
+        shapes_layer = napariViewer.layers[shapeLayer[0]]
+        # shapes_layer.text.values[0] = str(textv)
+
+            
 def getLayerIdFromName(layer_name):
     ImageLayer = [i for i, layer in enumerate(napariViewer.layers) if hasattr(layer, '_name') and layer._name == layer_name]
     return ImageLayer
@@ -58,15 +92,7 @@ def InitateNapariUI(napariViewer):
 def napariUpdateLive(liveImage):
     if liveImage is None:
         return
-
-    #Do analysis on this live image
-    # Check if the queue is empty
-    if image_queue.empty():
-        image_queue.put(liveImage)
-        # Start the analysis thread if it's not already running
-        if not analysis_thread.isRunning():
-            analysis_thread.start()
-    
+    print('NapariUpdateLive Ran')
     liveImageLayer = getLayerIdFromName('liveImage')
 
     #If it's the first liveImageLayer
@@ -77,37 +103,96 @@ def napariUpdateLive(liveImage):
         #Set correct scale - in nm
         layer.scale = [core.get_pixel_size_um(),core.get_pixel_size_um()]
         layer._keep_auto_contrast = True
-        napariViewer.reset_view()
         napariViewer.layers.move_multiple([nrLayersBefore,0])
+        napariViewer.reset_view()
     #Else if the layer already exists, replace it!
     else:
         # layer is present, replace its data
         layer = napariViewer.layers[liveImageLayer[0]]
         layer.data = liveImage
     
-@thread_worker(connect={'yielded': napariUpdateLive})
+    # Do analysis on this live image
+    # Check if the queue is empty
+    if image_queue_analysis.empty():
+        image_queue_analysis.put(liveImage)
+        # Start the analysis thread if it's not already running
+        if not analysis_thread.isRunning():
+            analysis_thread.start()
+
+class liveObtainThread(QThread):
+    liveObtain_done_signal = pyqtSignal(object)
+    
+    def run(self):
+        while True:
+            tagged_image = image_queue_liveObtain.get()
+            if isinstance(tagged_image, str):
+                print('STR')
+            else:
+                # get the pixels in numpy array and reshape it according to its height and width
+                image_array = np.reshape(
+                    tagged_image.pix,
+                    newshape=[-1, tagged_image.tags["Height"], tagged_image.tags["Width"]],
+                )
+
+                #When image is arrived from MM, we yield it to napariUpdateLive
+                self.liveObtain_done_signal.emit(image_array[0,:,:])
+                # yield image_array[0,:,:]
+        
+
+def updateNapariLiveView(liveImage):
+    napariUpdateLive(liveImage)
+    
+def dummyPrintSomething(im):
+    print('dummyPrintSomething')
+    
+@thread_worker(connect={'yielded': dummyPrintSomething})
+# @thread_worker()
 def buttonPressliveStateToggle():
+    print('buttonPressliveStateToggle run')
     global livestate, stop_continuous_task
-    print(livestate)
     if livestate:
         livestate = False
         stop_continuous_task = True
     else:
         livestate = True
         stop_continuous_task = False
-
+    print('livestate now ', livestate)
+    # if livestate == True:
+        # #Do analysis on this live image
+        # # Check if the queue is empty
+        
+    # Start the live obtain thread if it's not already running
+    if not liveObtain_thread.isRunning():
+        liveObtain_thread.start()
+    
+    # if livestate == True:
+        #Try to reset pycromanager somehow
+        # core.initialize_circular_buffer()
+        # core.wait_for_system()
+        # core = None
+        # core = Core()
+        #reset layer napari
+        # liveImageLayer = getLayerIdFromName('liveImage')
+        # print(liveImageLayer)
+        # if not liveImageLayer:
+        #     napariViewer.reset_view()
+        # else:
+        #     napariViewer.layers.remove(napariViewer.layers[liveImageLayer[0]])
+    
     while livestate == True:
         core.snap_image()
         tagged_image = core.get_tagged_image()
-        # get the pixels in numpy array and reshape it according to its height and width
-        image_array = np.reshape(
-            tagged_image.pix,
-            newshape=[-1, tagged_image.tags["Height"], tagged_image.tags["Width"]],
-        )
-        
-        #When image is arrived from MM, we yield it to napariUpdateLive
-        yield image_array[0,:,:]
- 
+        if image_queue_liveObtain.qsize()<10:
+            image_queue_liveObtain.put(tagged_image)
+            yield np.zeros((1))
+        else:
+            yield np.zeros((1))
+    
+    if livestate == False:
+        #Reset queues and exit threads
+        image_queue_liveObtain.queue.clear()
+        yield np.zeros((1))
+
 #This code gets some image and does some analysis on this
 class AnalysisThread(QThread):
     # Create a signal to communicate between threads
@@ -115,22 +200,21 @@ class AnalysisThread(QThread):
     
     def run(self):
         while True:
-            liveImage = image_queue.get()
-            time.sleep(1)
-            print('Some analysis done after sleeping?')
-            analysis_result = 10
+            liveImage = image_queue_analysis.get()
+            analysis_result = np.mean(np.mean(liveImage))
             self.analysis_done_signal.emit(analysis_result)
 
 def Visualise_Analysis_results(analysis_result):
     print(analysis_result)
-    drawRandomOverlay('live_overlay')
+    drawTextOverlay('Text_overlay',analysis_result)
+    # drawRandomOverlay('live_overlay')
 
 # Start a separate analysis thread
-# analysis_thread = threading.Thread(target=analysis_thread)
-# analysis_thread.daemon = True
-# analysis_thread.start()
 analysis_thread = AnalysisThread()
 analysis_thread.analysis_done_signal.connect(Visualise_Analysis_results)
+
+liveObtain_thread = liveObtainThread()
+liveObtain_thread.liveObtain_done_signal.connect(updateNapariLiveView)
 
 def runLiveModeUI(score,sMM_JSON,sform,sapp):
     #Go from self to global variables
