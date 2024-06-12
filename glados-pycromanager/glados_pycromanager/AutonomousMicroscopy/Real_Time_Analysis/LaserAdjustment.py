416f4cc42f41129b9888c99ba8ad7a1bfe8ef3fe
Koen J.A. Martens <32260034+kjamartens@users.noreply.github.com>
2024-06-12 14:23:13 +0200

    Added advanced laser adjustment JSON-ish-like #10

--- glados-pycromanager/glados_pycromanager/AutonomousMicroscopy/Real_Time_Analysis/LaserAdjustment.py
+++ glados-pycromanager/glados_pycromanager/AutonomousMicroscopy/Real_Time_Analysis/LaserAdjustment.py
@@ -12,13 +12,25 @@ def __function_metadata__():
     return {
         "laser_adjustment": {
             "required_kwargs": [
-                {"name": "Laser_id", "description": "LaserID", "default": 2, "type": int},
+                {"name": "Laser_id", "description": "LaserID", "default": "TS_DAC02_488Laser", "type": str},
                 {"name": "maxFrame", "description": "Max frames", "default": 100, "type": int}
             ],
             "optional_kwargs": [
             ],
             "help_string": ".",
             "display_name": "LaserAdj",
+            "run_delay": 0,
+            "visualise_delay": 500
+        },
+        "laser_adjustment_advanced": {
+            "required_kwargs": [
+                {"name": "Laser_id", "description": "LaserID", "default": "TS_DAC02_488Laser", "type": str},
+                {"name": "Laser_power", "description": "Laser power information", "default": "{1:0.01, 2:0.05, 10:1.00}", "type": str}
+            ],
+            "optional_kwargs": [
+            ],
+            "help_string": ".",
+            "display_name": "Advanced laser adjustment",
             "run_delay": 0,
             "visualise_delay": 500
         }
@@ -35,7 +47,7 @@ class laser_adjustment():
         class_name = inspect.currentframe().f_locals.get('self', None).__class__.__name__ #type:ignore
         [provided_optional_args, missing_optional_args] = FunctionHandling.argumentChecking(__function_metadata__(),class_name,kwargs) #type:ignore
 
-        propertyname = "TS_DAC02_488Laser"
+        propertyname = kwargs['Laser_id']
         MMprop_intensity_name = "Volts"
         ValIntMM = 0
         core.set_property(propertyname, MMprop_intensity_name, str(ValIntMM))
@@ -44,7 +56,7 @@ class laser_adjustment():
 
     def run(self,image,metadata,core,**kwargs):
         print("At frame: "+metadata['ImageNumber'])
-        propertyname = "TS_DAC02_488Laser"
+        propertyname = kwargs['Laser_id']
         MMprop_intensity_name = "Volts"
         frame = int(metadata['ImageNumber'])
         ValIntMM = min(5,frame/float(kwargs['maxFrame'])*5)
@@ -58,6 +70,53 @@ class laser_adjustment():
         # ValIntMM = ValIntPerc*float(MM_JSON["lasers"]["Laser"+str(laserID)]["Intensity_slope"])-float(MM_JSON["lasers"]["Laser"+str(laserID)]["Intensity_offset"])
         # #Set value in Micromanager
     
+    
+    def end(self,core,**kwargs):
+        print('end of laserAdj')
+        return
+    
+    def visualise(self,image,metadata,core,**kwargs):
+        print('optional visualising every time this is called!')
+
+
+
+class laser_adjustment_advanced():
+    def __init__(self,core,**kwargs):
+        print(core)
+        #Check if we have the required kwargs
+        class_name = inspect.currentframe().f_locals.get('self', None).__class__.__name__ #type:ignore
+        [provided_optional_args, missing_optional_args] = FunctionHandling.argumentChecking(__function_metadata__(),class_name,kwargs) #type:ignore
+
+
+        #Set the laser to 0
+        propertyname = kwargs['Laser_id']
+        MMprop_intensity_name = "Volts"
+        ValIntMM = 0
+        core.set_property(propertyname, MMprop_intensity_name, str(ValIntMM))
+        print('INIT laser_adjustment')
+        
+        
+        self.LaserKeys = list(eval(kwargs['Laser_power']+".keys()"))
+        print(self.LaserKeys)
+
+    def run(self,image,metadata,core,**kwargs):
+        print("At frame: "+metadata['ImageNumber'])
+        propertyname = kwargs['Laser_id']
+        MMprop_intensity_name = "Volts"
+        frame = int(metadata['ImageNumber'])
+        
+        ""
+        laserPower = eval(kwargs['Laser_power'])
+        
+        currentLaserPower=0
+        for i in reversed(range(len(laserPower))):
+            if frame+1 >= self.LaserKeys[i]:
+                currentLaserPower = laserPower[self.LaserKeys[i]]
+                print(currentLaserPower)
+                break
+        
+        ValIntMM = max(0,min(5,currentLaserPower*5))
+        core.set_property(propertyname, MMprop_intensity_name, str(ValIntMM))
     
     def end(self,core,**kwargs):
         print('end of laserAdj')
